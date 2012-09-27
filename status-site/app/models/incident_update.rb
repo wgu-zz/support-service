43054d1151e60468339b5536978fe99d14645c81
Glenn Oppegard <oppegard@gmail.com>
2012-09-27 12:50:08 -0700

    remove status-site from support-team repo
    Change-Id: Idb81c0daf2588de29abfdda59b670d039417ebe1

--- status-site/app/models/incident_update.rb
+++
@@ -1,6 +0,0 @@
-class IncidentUpdate < ActiveRecord::Base
-  attr_accessible :content, :title, :update_type
-  belongs_to :incident
-  belongs_to :creator, class_name: "User", foreign_key: "creator_user_id"
-  belongs_to :updater, class_name: "User", foreign_key: "updater_user_id"
-end
